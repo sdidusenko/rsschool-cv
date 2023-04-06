15c64b789f3fd3786da13594a131fb09e8c23c6b
Serhii Didusenko <didusenko40@gmail.com>
2023-04-07 02:41:55 +0300

    feat: add burger-menu on main page

--- shelter/pages/main/index.js
+++ shelter/pages/main/index.js
@@ -1,1 +1,34 @@
-console.log('По макету совпадает в соответствии/n с требованиями. Валидацию проходит./n Изза мелких неточностей оцениваю свою работу на 97%') 
+document.addEventListener("DOMContentLoaded", function () {
+ 
+  document.getElementById("burger").addEventListener("click", function () {
+    document.querySelector(".header").classList.toggle("open")
+  })
+});
+
+
+document.getElementById("burger").addEventListener("click", function () {
+  document.body.classList.toggle("lock")
+ })
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === "Escape") {
+       document.querySelector(".header").classList.remove("open")
+  }
+});
+
+document.getElementById("menu").addEventListener('click', event => {
+  event._isClickWithInMenu = true;
+});
+document.getElementById("burger").addEventListener('click', event => {
+  event._isClickWithInMenu = true;
+});
+
+document.body.addEventListener('click', event => {
+  if (event._isClickWithInMenu) return;
+  // Действие при клике
+  document.querySelector(".header").classList.remove("open")
+});
+
+
+
+  console.log('По макету совпадает в соответствии/n с требованиями. Валидацию проходит./n Изза мелких неточностей оцениваю свою работу на 97%') 
